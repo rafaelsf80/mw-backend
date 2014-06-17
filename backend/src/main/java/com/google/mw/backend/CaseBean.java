e8cec2b342c3fdffc76a6886ad6e528838c47e82
Matt Feigal <matt.feigal@gmail.com>
2014-06-17 10:45:56 +0200

    added Android Client files from Rafa's client project. added project level files to manage both backend and client

--- backend/src/main/java/com/google/mw/backend/CaseBean.java
+++ backend/src/main/java/com/google/mw/backend/CaseBean.java
@@ -21,21 +21,6 @@ public class CaseBean {
     private double latitude;
     private double longitude;
 
-    //need to establish rules for when to mark notificationSent true or false.
-    //presumably, tasks created by Mobile don't need a notification.
-    //Tasks set to Urgent by the web client should definitely send notification.
-    private Boolean notificationNeeded;
-//    private GeoPt geoPt;
-
-
-    public Boolean getNotificationNeeded() {
-        return notificationNeeded;
-    }
-
-    public void setNotificationNeeded(Boolean notificationNeeded) {
-        this.notificationNeeded = notificationNeeded;
-    }
-
     public long getId() {
         return id;
     }
--- backend/src/main/java/com/google/mw/backend/CaseEndpoint.java
+++ backend/src/main/java/com/google/mw/backend/CaseEndpoint.java
@@ -11,6 +11,7 @@ import com.google.appengine.api.datastore.EntityNotFoundException;
 import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PropertyProjection;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Transaction;
 
@@ -45,14 +46,20 @@ public class CaseEndpoint {
 
             caseEntity.setProperty("title", caseBean.getTitle());
             caseEntity.setProperty("owner", caseBean.getOwner());
-            caseEntity.setProperty("dateCreated", caseBean.getDateCreated());
+            if (caseBean.getDateCreated()==null) {
+                caseEntity.setProperty("dateCreated", new Date());
+            } else {
+                caseEntity.setProperty("dateCreated", caseBean.getDateCreated());
+            }
             caseEntity.setProperty("dateClosed", caseBean.getDateClosed());
-            caseEntity.setProperty("status", caseBean.getStatus());
+            if (caseBean.getStatus() == null) {
+                caseEntity.setProperty("status", "ACTIVE");
+            } else {
+                caseEntity.setProperty("status", caseBean.getStatus());
+            }
             caseEntity.setProperty("comments", caseBean.getComments());
             caseEntity.setProperty("latitude", caseBean.getLatitude());
             caseEntity.setProperty("longitude", caseBean.getLongitude());
-            caseEntity.setProperty("notificationNeeded", caseBean.getNotificationNeeded());
-//            caseEntity.setProperty("geoPt", caseBean.getGeoPt());
             datastoreService.put(caseEntity);
             txn.commit();
         } finally {
@@ -113,6 +120,28 @@ public class CaseEndpoint {
         return caseBeans;
     }
 
+    //Endpoints does not support returning Strings,
+    //therefore we will simply return a very empty "case" per owner
+    @ApiMethod(name = "getListOwners", path="getListOwners")
+    public List<CaseBean> getListOwners() {
+        DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
+        Query query = new Query("Case");
+        query.addProjection(new PropertyProjection("owner", String.class));
+        query.setDistinct(true);
+        query.addSort("owner");
+
+        List<Entity> results = datastoreService.prepare(query).asList(FetchOptions.Builder.withDefaults());
+        ArrayList<CaseBean> caseBeans = new ArrayList<CaseBean>();
+        for (Entity result : results) {
+            //CaseBean caseBean = convertEntitytoBean(result);
+            CaseBean caseBean = new CaseBean();
+            caseBean.setOwner(result.getProperty("owner").toString());
+            caseBeans.add(caseBean);
+        }
+
+        return caseBeans;
+    }
+
 
     @ApiMethod(name = "deleteCase")
     public void deleteCase(@Named("id") long id) {
@@ -150,71 +179,7 @@ public class CaseEndpoint {
         }
     }
 
-    //TODO: Discuss with Sandi, Nadia, Rafa the use case for this method
-    //Because Endpoints can't return a primitive (boolean), this method returns the
-    //List<CaseBean> and does NOT mark the cases as notification actually sent.
-    //Prefer to delete this method and just keep getAllNewNotifictions... do it in one call.
 
-    @ApiMethod(name = "hasNewNotification", path="notifications")
-    public List<CaseBean> hasNewNotification(@Named("owner") String owner) {
-        DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
-        Query query = new Query("Case");
-        Query.Filter ownerFilter = new Query.FilterPredicate("owner", Query.FilterOperator.EQUAL,
-                owner);
-        Query.Filter notificationNeededFilter = new Query.FilterPredicate("notificationNeeded", Query.FilterOperator.EQUAL,
-                true);
-        Query.Filter comboFilter = Query.CompositeFilterOperator.and(ownerFilter, notificationNeededFilter);
-        query.setFilter(comboFilter);
-        List<Entity> results = datastoreService.prepare(query).asList(FetchOptions.Builder.withDefaults());
-        //create a list of cases that will be sent as notifications
-        ArrayList<CaseBean> caseBeans = new ArrayList<CaseBean>();
-        for (Entity result : results) {
-            CaseBean caseBean = convertEntitytoBean(result);
-            caseBeans.add(caseBean);
-        }
-        return caseBeans;
-    }
-
-    //TODO: Discuss with Sandi & Rafa & Nadia, what is the use case for this method?
-    //Seems simpler just to use getMyCases every time.
-    @ApiMethod(name = "getAllNewNotifications", path="notifications")
-    public List<CaseBean> getAllNewNotifications(@Named("owner") String owner) {
-        DatastoreService datastoreService = DatastoreServiceFactory.getDatastoreService();
-        Query query = new Query("Case");
-        Query.Filter ownerFilter = new Query.FilterPredicate("owner", Query.FilterOperator.EQUAL,
-                owner);
-        Query.Filter notificationNeededFilter = new Query.FilterPredicate("notificationNeeded", Query.FilterOperator.EQUAL,
-                true);
-        Query.Filter comboFilter = Query.CompositeFilterOperator.and(ownerFilter, notificationNeededFilter);
-        query.setFilter(comboFilter);
-        List<Entity> results = datastoreService.prepare(query).asList(FetchOptions.Builder.withDefaults());
-
-        //create a list of cases that will be sent as notifications
-        ArrayList<CaseBean> caseBeans = new ArrayList<CaseBean>();
-        for (Entity result : results) {
-            CaseBean caseBean = convertEntitytoBean(result);
-            caseBeans.add(caseBean);
-        }
-
-        //Update the cases so they won't be re-notified
-        Transaction txn = datastoreService.beginTransaction();
-        try {
-            for (Entity result : results) {
-                result.setProperty("notificationNeeded", false);
-                datastoreService.put(result);
-            }
-            txn.commit();
-        } finally {
-            if (txn.isActive()) {
-                txn.rollback();
-            }
-        }
-
-
-        return caseBeans;
-
-
-    }
 
     private CaseBean convertEntitytoBean(Entity caseEntity) {
         //todo: need some validation / protection in case of null values.
@@ -230,7 +195,6 @@ public class CaseEndpoint {
         caseBean.setComments(getNullSafeString(caseEntity.getProperty("comments")));
         caseBean.setLatitude((Double) caseEntity.getProperty("latitude"));
         caseBean.setLongitude((Double) caseEntity.getProperty("longitude"));
-        caseBean.setNotificationNeeded((Boolean) caseEntity.getProperty("notificationNeeded"));
 //        GeoPt geoPt = new GeoPt((Float) caseEntity.getProperty("latitude"), (Float) caseEntity.getProperty("longitude"));
 //        caseBean.setGeoPt(geoPt);
 
@@ -246,6 +210,8 @@ public class CaseEndpoint {
             return o.toString();
         }
     }
+
+
 
 
 }
